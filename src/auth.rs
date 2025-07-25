db6ec7b50657013f3111f12f7cc4bab4051bba0c
paulwalk <paul@paulwalk.net>
2025-07-25 17:08:08 +0100

    added github webhook validation

--- src/auth.rs
+++ src/auth.rs
@@ -1,3 +1,6 @@
+use crate::server::HmacSha256;
+use axum::http::{HeaderMap};
+use hmac::Mac;
 use secrecy::SecretString;
 use std::env;
 use std::env::VarError;
@@ -9,6 +12,31 @@ pub fn get_secret_from_env(key: String) -> Result<SecretString, VarError> {
             log::error!("Error reading ENV variable with key: {} - {}", key, e);
             Err(e)
         }
+    }
+}
+
+pub fn validate_github_secret3(secret: &str, headers: HeaderMap, payload: String) -> Result<(), String> {
+    if let Some(in_sig) = headers.get("x-hub-signature-256") {
+        let signature = in_sig.to_str().unwrap();
+        match HmacSha256::new_from_slice(secret.as_bytes()) {
+            Ok(mut mac) => {
+                mac.update(payload.as_bytes());
+                if let Some(sig_sep) = signature.strip_prefix("sha256=") {
+                    match hex::decode(sig_sep) {
+                        Ok(decoded) => match mac.verify_slice(&decoded) {
+                            Ok(()) => Ok(()),
+                            Err(_) => Err(String::from("Failed to verify signature")),
+                        },
+                        Err(_) => Err(String::from("Couldn't decode secret")),
+                    }
+                } else {
+                    Err(String::from("could not parse x-hub-signature-256 as str"))
+                }
+            }
+            Err(_) => Err(String::from("could not build mac")),
+        }
+    } else {
+        Err(String::from("missing x-hub-signature-256"))
     }
 }
 
--- src/server.rs
+++ src/server.rs
@@ -1,14 +1,13 @@
+use crate::auth::validate_github_secret3;
 use axum::extract::State;
 use axum::response::{IntoResponse, Response};
 use axum::{
     Router,
-    body::{Body, Bytes},
-    extract::{Extension, Json, Path, Query, Request},
+    http::StatusCode,
     http::header::HeaderMap,
-    http::{StatusCode, header},
     routing::{get, post},
 };
-use hmac::{Hmac, Mac};
+use hmac::Hmac;
 use secrecy::{ExposeSecret, SecretString};
 use sha2::Sha256;
 
@@ -21,7 +20,8 @@ pub type HmacSha256 = Hmac<Sha256>;
 
 pub fn create_router(secret: SecretString) -> Router {
     let state = HookState { secret };
-    Router::new().route("/", get(|| async { "Hello, World!" }))
+    Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
         .route("/health", get(|| async { "OK" }))
         .route("/update", post(post_process_github_webhook))
         .with_state(state)
@@ -29,60 +29,19 @@ pub fn create_router(secret: SecretString) -> Router {
 
 pub async fn run_http_server(port: u16, secret: SecretString) {
     let router = create_router(secret);
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port))
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port)).await.unwrap();
     axum::serve(listener, router).await.unwrap();
 }
 
-async fn post_process_github_webhook(
-    State(state): State<HookState>,
-    headers: HeaderMap,
-    body: String,
-) -> Response {
-    if let Some(in_sig) = headers.get("x-hub-signature-256") {
-        let signature = in_sig.to_str().unwrap();
-        match HmacSha256::new_from_slice(state.secret.expose_secret().as_bytes()) {
-            Ok(mut mac) => {
-                mac.update(body.as_bytes());
-                if let Some(sig_sep) = signature.strip_prefix("sha256=") {
-                    match hex::decode(sig_sep) {
-                        Ok(decoded) => match mac.verify_slice(&decoded) {
-                            Ok(()) => {
-                                log::debug!("Successfully verified signature");
-                                (StatusCode::OK, "OK").into_response()
-                            }
-                            Err(_) => {
-                                log::error!("Failed to verify signature");
-                                (StatusCode::UNAUTHORIZED, "Invalid secret").into_response()
-                            }
-                        },
-                        Err(_) => {
-                            log::error!("Couldn't decode secret");
-                            (StatusCode::UNAUTHORIZED, "Couldn't decode secret").into_response()
-                        }
-                    }
-                } else {
-                    log::error!("could not parse x-hub-signature-256 as str");
-                    (
-                        StatusCode::BAD_REQUEST,
-                        "could not parse x-hub-signature-256 as str",
-                    )
-                        .into_response()
-                }
-            }
-            Err(e) => {
-                log::error!("could not build mac: {e}");
-                tracing::error!("could not build mac: {e}");
-                (
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                    "An error occured validating payload",
-                )
-                    .into_response()
-            }
+async fn post_process_github_webhook(State(state): State<HookState>, headers: HeaderMap, body: String) -> Response {
+    match validate_github_secret3(state.secret.expose_secret(), headers, body) {
+        Ok(_) => {
+            log::debug!("Successfully verified signature");
         }
-    } else {
-        log::error!("missing x-hub-signature-256");
-        (StatusCode::UNAUTHORIZED, "missing x-hub-signature-256").into_response()
+        Err(e) => {
+            log::error!("Failed to verify signature: {}", e);
+            return (StatusCode::UNAUTHORIZED, "Invalid secret").into_response();
+        }
     }
+    (StatusCode::OK, "OK").into_response()
 }
