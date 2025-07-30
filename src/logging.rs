34c1611fa707b74c92383d3598d075f06992c047
paulwalk <paul@paulwalk.net>
2025-07-30 10:06:35 +0100

    improved logging

--- src/logging.rs
+++ src/logging.rs
@@ -3,17 +3,15 @@ use log::Record;
 
 pub fn configure_flexi_logger(debug: bool) -> Result<(), Box<dyn std::error::Error>> {
     let logging_level = if debug { "debug" } else { "info" };
-
     flexi_logger::Logger::try_with_str(logging_level)?
         .log_to_stdout()
-        .set_palette("1;5;32;3;-".parse().unwrap())
-        .format(my_own_format)
+        .set_palette("1;5;32;3;-".parse()?)
+        .format(custom_logging_format)
         .start()?;
-
     Ok(())
 }
 
-pub fn my_own_format(
+pub fn custom_logging_format(
     w: &mut dyn std::io::Write,
     _now: &mut DeferredNow,
     record: &Record,
